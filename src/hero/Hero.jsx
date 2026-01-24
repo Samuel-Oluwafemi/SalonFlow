d89d3a40b408216c9a0b5590957306bd09eb640e
oluwafemix <samuelkeller47@gmail.com>
2026-01-24 19:46:27 +0100

    Subtext update

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -7,10 +7,10 @@ export default function Hero() {
       <Navbar />
       <div className="min-h-screen bg-white pt-28  text-center">
         <h1 className="text-4xl font-bold text-black mb-4 md:text-5xl px-14 md:px-8">
-          A Smarter Way for Salons to Manage Appointments
+          A Smarter Way for Brands to Manage Appointments
         </h1>
         <p className="text-lg text-gray-700 mb-6 md:text-xl px-5 md:px-0">
-          Designed to help salons manage bookings, save time, and never miss appointments.
+          Designed to help brands manage bookings, save time, and never miss appointments.
         </p>
         <a
           href="#services"
