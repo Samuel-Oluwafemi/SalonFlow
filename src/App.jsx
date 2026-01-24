91b61834f4bda664ef88ac45488644a6bff23122
oluwafemix <samuelkeller47@gmail.com>
2026-01-24 23:40:55 +0100

    Added navlinks for quick movement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Hero from "./hero/Hero"
+import Hero from "./hero/Hero";
+import { Services } from "./BookingFlow/Services";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <div classname="bg-white max-w-md mx-auto ">
-        <Hero />
-        </div>
+          <Routes>
+            <Route path="/" element={<Hero />} />
+            <Route path="/services" element={<Services />} />
+          </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,23 +1,25 @@
 import { Navbar } from "../Navbar/Navbar";
 import { Img } from "./heroImg";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
     <section>
       <Navbar />
-      <div className="min-h-screen bg-white pt-28  text-center">
-        <h1 className="text-4xl font-bold text-black mb-4 md:text-5xl px-14 md:px-8">
+      <div className="min-h-screen bg-white pt-28 text-center">
+        <h1 className="text-4xl md:text-6xl md:mt-4 font-bold text-black mb-4 px-14 md:px-23">
           A Smarter Way for Brands to Manage Appointments
         </h1>
         <p className="text-lg text-gray-700 mb-6 md:text-xl px-5 md:px-0">
-          Designed to help brands manage bookings, save time, and never miss appointments.
+          Designed to help brands manage bookings, save time, and never miss
+          appointments.
         </p>
-        <a
-          href="#services"
-          className="inline-block bg-purple-600 text-white font-semibold py-3 px-6 rounded-full hover:bg-purple-700 transition-colors duration-300"
-        >
-          Explore Our Services
-        </a>
+        <Link to="/services" target="_blank" rel="noopener noreferrer">
+          <button className="bg-purple-600 text-white px-6 py-3 rounded-md text-lg 
+          font-semibold hover:bg-purple-700 transition duration-300">
+            Book a Demo
+          </button>
+        </Link>
         <Img />
       </div>
     </section>
