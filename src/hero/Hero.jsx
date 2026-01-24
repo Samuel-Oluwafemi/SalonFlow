27685d424fc58856b67cd7b1fb06fc9e0504e67d
oluwafemix <samuelkeller47@gmail.com>
2026-01-24 18:11:00 +0100

    Added hero section for salonflow, illustration

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,22 +1,25 @@
 import { Navbar } from "../Navbar/Navbar";
+import { Img } from "./heroImg";
+
 export default function Hero() {
-    return (
-        <section>
-            <Navbar />
-            <div className="bg-white pt-24 pb-12 px-4 text-center md:px-8">
-                <h1 className="text-4xl font-bold text-purple-600 mb-4 md:text-5xl">
-                    Welcome to SalonFlow    
-                </h1>
-                <p className="text-lg text-gray-700 mb-6 md:text-xl">
-                    Your Ultimate Salon Booking Experience
-                </p>
-                <a
-                    href="#services"
-                    className="inline-block bg-purple-600 text-white font-semibold py-3 px-6 rounded-full hover:bg-purple-700 transition-colors duration-300"
-                >
-                    Explore Our Services
-                </a>
-            </div>
-        </section>
-    )
-} 
+  return (
+    <section>
+      <Navbar />
+      <div className="min-h-screen bg-white pt-28  text-center">
+        <h1 className="text-4xl font-bold text-black mb-4 md:text-5xl px-14 md:px-8">
+          A Smarter Way for Salons to Manage Appointments
+        </h1>
+        <p className="text-lg text-gray-700 mb-6 md:text-xl px-5 md:px-0">
+          Designed to help salons manage bookings, save time, and never miss appointments.
+        </p>
+        <a
+          href="#services"
+          className="inline-block bg-purple-600 text-white font-semibold py-3 px-6 rounded-full hover:bg-purple-700 transition-colors duration-300"
+        >
+          Explore Our Services
+        </a>
+        <Img />
+      </div>
+    </section>
+  );
+}
