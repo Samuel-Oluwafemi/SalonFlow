f7e4217a526e2c1853ad4bf003deca0295414412
oluwafemix <samuelkeller47@gmail.com>
2026-02-04 00:50:13 +0100

    route path changed to the parent element

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <div classname="bg-white max-w-md mx-auto ">
+      <div className="bg-white mx-auto ">
           <Routes>
             <Route path="/" element={<Hero />} />
             <Route path="/services" element={<BookingFlow />} />
           </Routes>
-
-          <BookingFlow />
       </div>
     </>
   );
