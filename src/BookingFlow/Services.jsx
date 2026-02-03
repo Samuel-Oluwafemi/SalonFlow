b8a93587672e66d60fed4b1369bea757137520e2
oluwafemix <samuelkeller47@gmail.com>
2026-02-04 00:54:51 +0100

    Step 2 of service selection + added a continue btn when service = selected

--- src/BookingFlow/Services.jsx
+++ src/BookingFlow/Services.jsx
@@ -1,60 +1,65 @@
 import { Navbar } from "../Navbar/Navbar";
-export function Services() {
-  const services = [
-    {
-      id: 1,
-      name: "Wig Installation",
-      duration: "2hrs",
-      price: "#25k",
-      image: "/images/elless wigs.jpeg"
-    },
-    {
-      id: 2,
-      name: "Braiding",
-      duration: "3hrs",
-      price: "#30k",
-      image: "/images/elless braiding2.jpeg"
-    },
-    {
-      id: "3",
-      name: "Pedicure & Manicure",
-      duration: "1hr",
-      price: "#15k",
-      image: "/images/elless pedicure.jpeg"
-    },
-    {
-      id: 4,
-      name: "Tooth Gem Application",
-      duration: "2hrs",
-      price: "#20k",
-      image: "/images/elless Tooth Gem.jpeg"
-    }
-  ];
+const Services = ({
+  // props
+  services,
+  selectedService,
+  onSelectedService,
+  onContinue,
+}) => {
+  // Continue btn is enabled only if a service is selected and is available
+  const canContinue = Boolean(selectedService);
+  // error logging for debugging
+  if (!canContinue) {
+    console.log("Continue button is disabled");
+  } else {
+    console.log("Continue button is enabled");
+  }
   return (
     <section className="min-h-screen mx-auto py-10 pt-28">
       <Navbar />
-      <h1 className="text-2xl md:text-3xl text-center font-bold mb-6">Categories</h1>
-      <div className="grid grid-cols-2 gap-4 md:gap-10 px-2 md:px-10">
-        {/* Loop through service categories */}
-        {services.map((service) => (
-          // service id as key
-          <div key={service.id} 
-          className="p-1 md:p-2 rounded-3xl hover:shadow-lg 
-          md:hover:shadow-md hover:shadow-purple-400 transition-shadow duration-300 text-center">
-            {/* service image */}
-            <div className="w-full">
-              <img src={service.image} alt={service.title} 
-              className="w-full h-50 md:h-70 relative transition-transform duration-300 
-              rounded-3xl object-cover p-2"/>
-            </div>
-            {/* service title */}
-            <div className="p-2">
-            <h2 className="text-lg font-semibold mb-2">{service.name}</h2>
-          </div>
-          </div>
+      <h1 className="text-2xl md:text-3xl text-center font-bold mb-8">
+        Categories of Services
+      </h1>
 
-        ))}
+      <div className="grid grid-cols-2 md:grid-cols-4 md:gap-4 gap-6 px-2 md:px-10">
+        {/* Loop through service categories */}
+        {services.map((service) => {
+          // for every service, check if it's selected
+          const isSelected = selectedService?.id === service.id;
+
+          return (
+            <button
+              key={service.id}
+              onClick={() => onSelectedService(service)}
+              className={`p-1 md:p-2 rounded-3xl hover:shadow-lg transition-duration-300 text-center 
+                ${isSelected ? "shadow-lg shadow-purple-400" : "border border-gray-300"}`}
+            >
+              {/* service image */}
+              <div className="flex justify-center">
+                <img
+                  src={service.image}
+                  alt={service.name}
+                  className="h-20 w-20 md:h-24 md:w-24 object-cover rounded-full"
+                />
+              </div>
+              <div className="p-2">
+                <p className="text-lg font-semibold mb-2">{service.name}</p>
+              </div>
+            </button>
+          );
+        })}
+      </div>
+      <div className="flex justify-center px-10 md:px-20">
+        <button
+          disabled={!canContinue}
+          onClick={onContinue}
+          className={`mt-10 w-100 py-3 rounded-full font-semibold text-lg 
+            ${canContinue ? "bg-purple-500 text-white cursor-pointer" : "bg-gray-300 text-gray-600 cursor-not-allowed"}`}
+        >
+          Continue
+        </button>
       </div>
     </section>
   );
-}
+};
+export default Services;
