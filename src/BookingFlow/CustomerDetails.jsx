c5340ca078a3d3760a839c1a22b60fb56f240cad
oluwafemix <samuelkeller47@gmail.com>
2026-02-08 20:43:49 +0100

    delete

--- src/BookingFlow/CustomerDetails.jsx
+++
@@ -1,9 +0,0 @@
-import { Navbar } from "../Navbar/Navbar";
-const CustomerDetails = () => {
-  return (
-    <section className="min-h-screen mx-auto py-10 pt-28">
-      <Navbar />
-    </section>
-  );
-};
-export default CustomerDetails;
