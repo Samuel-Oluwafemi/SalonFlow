903f5797e7e48822ed87a98d3211748edad00f23
oluwafemix <samuelkeller47@gmail.com>
2026-02-10 01:20:14 +0100

    applied framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 import Hero from "./hero/Hero";
 import BookingFlow from "./BookingFlow/BookingFlow";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 function App() {
+   const location = useLocation()
   return (
     <>
       <div className="max-w-7xl bg-white mx-auto ">
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/services" element={<BookingFlow />} />
+        <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.4 }}
+              >
+                <Hero />
+              </motion.div>
+            }
+          />
+
+          <Route
+            path="/services"
+            element={
+              <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 0.4 }}
+              >
+                <BookingFlow />
+              </motion.div>
+            }
+          />
         </Routes>
+      </AnimatePresence>
       </div>
     </>
   );
