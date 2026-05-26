abf3097a85fcd6e7f014471361b76b864d704406
oluwafemix <samuelkeller47@gmail.com>
2026-05-26 22:27:55 +0100

    new fonts display

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ export default {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+        sora: ["Sora", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
       },
     },
   },
